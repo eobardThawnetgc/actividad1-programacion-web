5b855f3bce690445cddb0f4bd19063596f32f3e8
sofircds <00175525@uca.edu.sv>
2026-09-17 13:15:36 -0600

    feat: conectar endpoints de eliminar incidencia y estadisticas

--- routes/incidencias.js
+++ routes/incidencias.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import { crearIncidencia } from '../controllers/registrarIncidencias.js';
-import { listarIncidencias, buscarIncidenciaPorId, clasificarIncidencia } from '../controllers/consultarIncidencias.js';
+import { listarIncidencias, buscarIncidenciaPorId, clasificarIncidencia, eliminarIncidencia, obtenerEstadisticas } from '../controllers/consultarIncidencias.js';
 
-const router = express.Router();
+const router = express.Router(); 
 
 //funcion solo para poner en las rutas mientras se crean los controladores
 const funcionRelleno = () => {};
 
 router.get('/incidencias', listarIncidencias);
+router.get('/estadisticas', obtenerEstadisticas);
 router.get('/incidencias/:id', buscarIncidenciaPorId);
-router.get('/estadisticas', funcionRelleno);
 router.get('/estadisticas/:id/clasificacion', clasificarIncidencia);
 router.post('/incidencias', crearIncidencia); //crear incidencia
 router.put('/incidencias/:id/estado', funcionRelleno); //cambiar estado de incidencia
-router.delete('/incidencias/:id', funcionRelleno);
+router.delete('/incidencias/:id', eliminarIncidencia);
 
 export default router;
